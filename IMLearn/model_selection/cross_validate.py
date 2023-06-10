dbd789b8a12fca293b1bdc72dfcee41da6712a13
ErelNahum <erelnahum2003@gmail.com>
2023-06-10 20:30:24 +0300

    Ex4 - Cross Validate

--- IMLearn/model_selection/cross_validate.py
+++ IMLearn/model_selection/cross_validate.py
@@ -37,4 +37,17 @@ def cross_validate(estimator: BaseEstimator, X: np.ndarray, y: np.ndarray,
     validation_score: float
         Average validation score over folds
     """
-    raise NotImplementedError()
+    indices = np.arange(X.shape[0])
+    folds = np.array_split(indices, cv)
+
+    train_score = 0.0
+    validation_score = 0.0
+
+    for fold_indices in folds:
+        train_msk = ~np.isin(indices, fold_indices)
+        fit = deepcopy(estimator).fit(X[train_msk], y[train_msk])
+
+        train_score += scoring(y[train_msk], fit.predict(X[train_msk]))
+        validation_score += scoring(y[fold_indices], fit.predict(X[fold_indices]))
+
+    return train_score / cv, validation_score / cv
